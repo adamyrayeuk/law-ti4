7698f405227f526092a30bbab3ab2ca4ce9b4b67
Muhamad Adamy Rayeuk <muhamad.adamy@ui.ac.id>
2022-06-12 13:50:50 +0700

    Create mahasiswa model and read endpoint

--- read/read/urls.py
+++ read/read/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from mahasiswa.views import read
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('read/<str:id>', read)
 ]
